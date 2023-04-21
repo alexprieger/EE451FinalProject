223d45d46568fabea27b59f7fafd543676e70d1c
Alex Preger <prieger@usc.edu>
2023-04-21 11:39:17 -0700

    Implemented simplified Suzuki algorithm

--- app/src/main/cpp/compute.cpp
+++ app/src/main/cpp/compute.cpp
@@ -1,5 +1,7 @@
 #include <jni.h>
+#include <android/log.h>
 #include <string>
+#include <sstream>
 #include <vulkan/vulkan.h>
 
 extern "C" JNIEXPORT jstring JNICALL
@@ -9,6 +11,7 @@ Java_com_ajp_ee451finalproject_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
+
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_ajp_ee451finalproject_MainActivity_vulkanHello(JNIEnv *env, jobject thiz) {
@@ -28,4 +31,149 @@ Java_com_ajp_ee451finalproject_MainActivity_vulkanHello(JNIEnv *env, jobject thi
     VkInstance instance;
 
     VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
+}
+
+enum Direction {
+    left = 0,
+    up = 1,
+    right = 2,
+    down = 3
+};
+
+struct element {
+    int row;
+    int col;
+    Direction direction;
+};
+
+Direction nextClockWiseDirection(Direction startDirection) {
+    return static_cast<Direction>((startDirection + 1) % 4);
+}
+
+Direction nextCounterClockWiseDirection(Direction startDirection) {
+    // Add 3 instead of subtracting 1 to avoid negative modulus weirdness
+    return static_cast<Direction>((startDirection + 3) % 4);
+}
+
+struct element scanClockwise(const jbyte* imageBuffer, jint width, int row, int col, Direction startDirection) {
+    Direction nextDirection = nextClockWiseDirection(startDirection);
+    while (nextDirection != startDirection) {
+        switch (nextDirection) {
+            case left:
+                if (imageBuffer[row * width + col - 1] != 0) {
+                    return {row, col - 1, left};
+                }
+                break;
+            case up:
+                if (imageBuffer[(row - 1) * width + col] != 0) {
+                    return {row - 1, col, up};
+                }
+                break;
+            case right:
+                if (imageBuffer[row * width + col + 1] != 0) {
+                    return {row, col + 1, right};
+                }
+                break;
+            case down:
+                if (imageBuffer[(row + 1) * width + col] != 0) {
+                    return {row + 1, col, down};
+                }
+                break;
+        }
+        nextDirection = nextClockWiseDirection(nextDirection);
+    }
+    return {0, 0}; // No pixel found: (0, 0) cannot possibly be a neighbor of a 1-pixel
+}
+
+struct element scanCounterClockwise(const jbyte* imageBuffer, jint width, int row, int col, Direction startDirection, bool& isBorderingToRight) {
+    isBorderingToRight = false;
+    Direction nextDirection = nextCounterClockWiseDirection(startDirection);
+    while (true) { // One of these is guaranteed to be hit within four tries
+        switch (nextDirection) {
+            case left:
+                if (imageBuffer[row * width + col - 1] != 0) {
+                    return {row, col - 1, right};
+                }
+                break;
+            case up:
+                if (imageBuffer[(row - 1) * width + col] != 0) {
+                    return {row - 1, col, down};
+                }
+                break;
+            case right:
+                if (imageBuffer[row * width + col + 1] != 0) {
+                    return {row, col + 1, left};
+                } else {
+                    isBorderingToRight = true;
+                }
+                break;
+            case down:
+                if (imageBuffer[(row + 1) * width + col] != 0) {
+                    return {row + 1, col, up};
+                }
+                break;
+        }
+        nextDirection = nextCounterClockWiseDirection(nextDirection);
+    }
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_ajp_ee451finalproject_MainActivity_processImageBitmap(JNIEnv *env, jobject thiz,
+                                                               jbyteArray image, jint width,
+                                                               jint height) {
+    // Image array is in row major order, with 0 representing black and 1 representing white
+    jbyte* imageBuffer = env->GetByteArrayElements(image, nullptr);
+    int numberOfBorders = 1;
+    // Perform a raster scan of the entire image.
+    // We do not iterate over the (entirely black) frame, which conveniently avoids wraparound issues
+    for (int row = 1; row < height - 1; row++) {
+        for (int col = 1; col < width - 1; col++) {
+            struct element startPixel = {row, col};
+            struct element zeroPixel{};
+            if (imageBuffer[row * width + col] == 1 && imageBuffer[row * width + col - 1] == 0) {
+                zeroPixel = {row, col - 1, left};
+            } else if (imageBuffer[row * width + col] >= 1 && imageBuffer[row * width + col + 1] == 0) {
+                zeroPixel = {row, col + 1, right};
+            } else {
+                continue;
+            }
+            numberOfBorders++;
+            // Border following initialization: find the first two pixels
+            struct element startPrevPixel = scanClockwise(imageBuffer, width, row, col, zeroPixel.direction);
+            if (startPrevPixel.row == 0 && startPrevPixel.col == 0) {
+                continue;
+            }
+            struct element currentPixel = startPixel;
+            currentPixel.direction = startPrevPixel.direction;
+            struct element prevPixel = startPrevPixel;
+            // Now following a border
+            while (true) {
+                bool isBorderingToRight;
+                struct element nextPixel = scanCounterClockwise(imageBuffer, width, currentPixel.row, currentPixel.col, currentPixel.direction, isBorderingToRight);
+                if (isBorderingToRight) {
+                    // We need to specially mark the pixel if it's the border we're currently following
+                    // faces to the right, to prevent us from accidentally following it again assuming it's a hole border
+                    imageBuffer[currentPixel.row * width + currentPixel.col] = -numberOfBorders;
+                } else if (imageBuffer[currentPixel.row * width + currentPixel.col] == 1) {
+                    imageBuffer[currentPixel.row * width + currentPixel.col] = numberOfBorders;
+                }
+                // If we're back at the starting point, we've found the entire border
+                if (nextPixel.row == startPixel.row && nextPixel.col == startPixel.col
+                    && currentPixel.row == startPrevPixel.row && currentPixel.col == startPrevPixel.col) {
+                    break;
+                }
+                prevPixel = currentPixel;
+                currentPixel = nextPixel;
+            }
+        }
+    }
+    for (int row = 1; row < height - 1; row++) {
+        std::stringstream rowString;
+        for (int col = 1; col < width - 1; col++) {
+            rowString << static_cast<int>(imageBuffer[row * width + col]) << " ";
+        }
+        __android_log_print(ANDROID_LOG_INFO, "Alex", "%s", rowString.str().c_str());
+    }
+    env->ReleaseByteArrayElements(image, imageBuffer, JNI_ABORT);
 }
--- app/src/main/java/com/ajp/ee451finalproject/MainActivity.kt
+++ app/src/main/java/com/ajp/ee451finalproject/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.ajp.ee451finalproject
 
+import android.content.res.AssetManager.ACCESS_BUFFER
+import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.graphics.Color.rgb
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import com.ajp.ee451finalproject.databinding.ActivityMainBinding
+import java.io.BufferedInputStream
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +24,17 @@ class MainActivity : AppCompatActivity() {
 
         // Example of a call to a native method
         binding.sampleText.text = stringFromJNI()
-        vulkanHello()
+
+        val imageStream = BufferedInputStream(resources.assets.open("images/waffle.png", ACCESS_BUFFER))
+        val imageBitmap = BitmapFactory.decodeStream(imageStream)
+        // Image array is in row major order, with 0 representing black and 1 representing white
+        val imageArray = ByteArray(imageBitmap.width * imageBitmap.height) { i ->
+            val row = i / imageBitmap.width
+            val col = i % imageBitmap.width
+            // Doing a bit of a transformation here between (x,y) coordinates and standard row-major matrix index ordering
+            if (imageBitmap.getPixel(col, row) == rgb(0, 0, 0)) 0 else 1
+        }
+        processImageBitmap(imageArray, imageBitmap.width, imageBitmap.height)
     }
 
     /**
@@ -27,6 +44,8 @@ class MainActivity : AppCompatActivity() {
     external fun stringFromJNI(): String
 
     external fun vulkanHello()
+
+    external fun processImageBitmap(image: ByteArray, width: Int, height: Int)
 
     companion object {
         // Used to load the 'ee451finalproject' library on application startup.
