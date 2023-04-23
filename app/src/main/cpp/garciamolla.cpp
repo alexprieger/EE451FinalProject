f2c9451dfe5791af149cd3cdedf925d101cc4964
Alex Prieger <prieger@usc.edu>
2023-04-22 17:00:24 -0700

    Correctly implemented border finding base case of Garcia-Molla

--- app/src/main/cpp/garciamolla.cpp
+++ app/src/main/cpp/garciamolla.cpp
@@ -69,7 +69,14 @@ struct PixelDirection scan(jbyte* imageBuffer, jint width, Pixel centerPixel, Di
     } else {
         nextDirection = nextCounterClockWiseDirection(startDirection);
     }
-    while (nextDirection != startDirection) {
+    for (int i = 0; i <
+#ifdef CONNECTIVITY_8
+        8
+#endif
+#ifndef CONNECTIVITY_8
+        4
+#endif
+    ; i++) {
         switch (nextDirection) {
             case left:
                 imageBuffer[centerPixel.row * width + centerPixel.col] |= LEFT_MASK;
@@ -127,64 +134,104 @@ struct PixelDirection scan(jbyte* imageBuffer, jint width, Pixel centerPixel, Di
     return {0, 0}; // No pixel found: (0, 0) cannot possibly be a 1-pixel
 }
 
-std::list<Triad> followBorder(jbyte* imageBuffer, jint width, int startRow, int endRow, int startCol, int endCol, Triad startTriad) {
-    std::list<Triad> border;
-    border.push_back(startTriad);
+std::list<Triad>* followBorder(jbyte* imageBuffer, jint width, int startRow, int endRow, int startCol, int endCol, Triad startTriad) {
+    auto* border = new std::list<Triad>();
+    border->push_back(startTriad);
     Triad currentTriad = startTriad;
+    bool finished = false;
     // Follow border forward until we leave our segment or loop back around
     while (currentTriad.nextPixel.pixel.row >= startRow && currentTriad.nextPixel.pixel.row < endRow
            && currentTriad.nextPixel.pixel.col >= startCol && currentTriad.nextPixel.pixel.col < endCol
-           && !(currentTriad.currentPixel.row == startTriad.currentPixel.row
-            && currentTriad.currentPixel.col == startTriad.currentPixel.col
-            && currentTriad.nextPixel.direction == startTriad.nextPixel.direction)) {
+           && !finished) {
         PixelDirection nextNextPixel = scan(imageBuffer, width, currentTriad.nextPixel.pixel, currentTriad.nextPixel.direction, false);
         currentTriad = {currentTriad.nextPixel.pixel,
                         {currentTriad.currentPixel,reverseDirection(currentTriad.nextPixel.direction)},
                         nextNextPixel};
-        border.push_back(currentTriad);
+        border->push_back(currentTriad);
+        finished = (currentTriad.currentPixel.row == startTriad.currentPixel.row
+                    && currentTriad.currentPixel.col == startTriad.currentPixel.col
+                    && currentTriad.nextPixel.direction == startTriad.nextPixel.direction);
     }
-    // If the loop is not closed yet, follow border backward
-    if (!(border.front().currentPixel.row == border.back().currentPixel.row
-         && border.front().currentPixel.col == border.back().currentPixel.col
-         && border.front().nextPixel.direction == border.back().nextPixel.direction)) {
-        // Follow border backward until we leave our segment or loop back around
-        while (currentTriad.nextPixel.pixel.row >= startRow && currentTriad.nextPixel.pixel.row < endRow
-               && currentTriad.nextPixel.pixel.col >= startCol && currentTriad.nextPixel.pixel.col < endCol
-               && !(currentTriad.currentPixel.row == border.back().currentPixel.row
-                    && currentTriad.currentPixel.col == border.back().currentPixel.col
-                    && currentTriad.nextPixel.direction == border.back().nextPixel.direction)) {
-            PixelDirection prevPrevPixel = scan(imageBuffer, width, currentTriad.prevPixel.pixel, currentTriad.prevPixel.direction, true);
-            currentTriad = {currentTriad.prevPixel.pixel,
-                            {currentTriad.currentPixel, (currentTriad.prevPixel.direction)},
-                            prevPrevPixel};
-            border.push_back(currentTriad);
-        }
+    // Follow border backward until we leave our segment or loop back around
+    // If we already looped, while condition will evaluate false and never run
+    currentTriad = startTriad;
+    while (currentTriad.prevPixel.pixel.row >= startRow && currentTriad.prevPixel.pixel.row < endRow
+           && currentTriad.prevPixel.pixel.col >= startCol && currentTriad.prevPixel.pixel.col < endCol
+           && !(currentTriad.currentPixel.row == border->back().currentPixel.row
+                && currentTriad.currentPixel.col == border->back().currentPixel.col
+                && currentTriad.prevPixel.direction == border->back().prevPixel.direction)) {
+        PixelDirection prevPrevPixel = scan(imageBuffer, width, currentTriad.prevPixel.pixel, currentTriad.prevPixel.direction, true);
+        currentTriad = {currentTriad.prevPixel.pixel,
+                        prevPrevPixel,
+                        {currentTriad.currentPixel, reverseDirection(currentTriad.prevPixel.direction)}};
+        border->push_front(currentTriad);
     }
     return border;
 }
 
-bool checkTriad(jbyte* imageBuffer, jint width, int startRow, int endRow, int startCol, int endCol, int row, int col, std::list<std::list<Triad>>& edgeList, jbyte triadBitmask, Direction triadDirection) {
+struct BorderList {
+    std::list<std::list<Triad> *>* closedBorders;
+    std::list<std::list<Triad> *>* openBorders;
+};
+
+bool isBorderClosed(std::list<Triad>* border) {
+    return
+    // Border is just a single pixel
+    (border->size() == 1 && border->front().nextPixel.pixel.row == 0)
+    // ...or the border is a closed loop
+    || (border->size() > 1
+    && border->front().currentPixel.row == border->back().currentPixel.row
+    && border->front().currentPixel.col == border->back().currentPixel.col
+    && border->front().nextPixel.direction == border->back().nextPixel.direction);
+}
+
+bool checkTriad(jbyte* imageBuffer, jint width, int startRow, int endRow, int startCol, int endCol, int row, int col, BorderList edgeList, jbyte triadBitmask, Direction triadDirection) {
     Pixel currentPixel = {row, col};
+    switch (triadDirection) {
+        case left:
+            if (imageBuffer[row * width + col - 1] != 0){
+                return false;
+            }
+            break;
+        case up:
+            if (imageBuffer[(row - 1) * width + col] != 0){
+                return false;
+            }
+            break;
+        case right:
+            if (imageBuffer[row * width + col + 1] != 0){
+                return false;
+            }
+            break;
+        case down:
+            if (imageBuffer[(row + 1) * width + col - 1] != 0){
+                return false;
+            }
+            break;
+    }
     if (!(imageBuffer[row * width + col] & triadBitmask)) {
         imageBuffer[row * width + col] |= triadBitmask;
         PixelDirection nextPixel = scan(imageBuffer, width, currentPixel, triadDirection, false);
         if (nextPixel.pixel.row == 0 && nextPixel.pixel.col == 0) {
             // No other pixel found, pixel is isolated
-            edgeList.emplace_back();
-            edgeList.back().push_back({currentPixel});
+            edgeList.closedBorders->push_back(new std::list<Triad>());
+            edgeList.closedBorders->back()->push_back({currentPixel});
             return true;
         }
         PixelDirection prevPixel = scan(imageBuffer, width, currentPixel, triadDirection, true);
         Triad pixelTriad = {currentPixel, prevPixel, nextPixel};
-        edgeList.push_back(
-                followBorder(imageBuffer, width, startRow, endRow, startCol, endCol,
-                             pixelTriad));
+        auto* border = followBorder(imageBuffer, width, startRow, endRow, startCol, endCol,pixelTriad);
+        if (isBorderClosed(border)) {
+            edgeList.closedBorders->push_back(border);
+        } else {
+            edgeList.openBorders->push_back(border);
+        }
     }
     return false;
 }
 
-std::list<std::list<Triad>> littleSuzuki(jbyte* imageBuffer, jint width, int startRow, int endRow, int startCol, int endCol) {
-    std::list<std::list<Triad>> edgeList;
+BorderList littleSuzuki(jbyte* imageBuffer, jint width, int startRow, int endRow, int startCol, int endCol) {
+    BorderList edgeList = {new std::list<std::list<Triad> *>(), new std::list<std::list<Triad> *>()};
     for (int row = startRow; row < endRow; row++) {
         for (int col = startCol; col < endCol; col++) {
             if (imageBuffer[row * width + col] == 0) {
@@ -208,24 +255,23 @@ std::list<std::list<Triad>> littleSuzuki(jbyte* imageBuffer, jint width, int sta
             }
         }
     }
+    return edgeList;
 }
 
-std::list<std::list<Triad>> findBordersInRectangle(jbyte* imageBuffer, jint width, int startRow, int endRow, int startCol, int endCol) {
+BorderList findBordersInRectangle(jbyte* imageBuffer, jint width, int startRow, int endRow, int startCol, int endCol) {
     if (endRow - startRow <= 4 && endCol - startCol <= 4) {
-        return {};
+        return littleSuzuki(imageBuffer, width, startRow, endRow, startCol, endCol);
     }
     // Process block [startRow, endRow) × [startCol, endCol)
     if (endRow - startRow >= endCol - startCol) {
         // More rows than columns, so split vertically
-        std::list<std::list<Triad>> list1 = findBordersInRectangle(imageBuffer, width, startRow, (startRow + endRow) / 2, startCol, endCol);
-        std::list<std::list<Triad>> list2 = findBordersInRectangle(imageBuffer, width, (startRow + endRow) / 2, endRow, startCol, endCol);
-        list1.splice(list1.end(), list2);
+        BorderList list1 = findBordersInRectangle(imageBuffer, width, startRow, (startRow + endRow) / 2, startCol, endCol);
+        BorderList list2 = findBordersInRectangle(imageBuffer, width, (startRow + endRow) / 2, endRow, startCol, endCol);
         return list1;
     }
     // More columns than rows, so split horizontally
-    std::list<std::list<Triad>> list1 = findBordersInRectangle(imageBuffer, width, startRow, endRow, startCol, (startCol + endCol) / 2);
-    std::list<std::list<Triad>> list2 = findBordersInRectangle(imageBuffer, width, startRow, endRow, (startCol + endCol) / 2, endCol);
-    list1.splice(list1.end(), list2);
+    BorderList list1 = findBordersInRectangle(imageBuffer, width, startRow, endRow, startCol, (startCol + endCol) / 2);
+    BorderList list2 = findBordersInRectangle(imageBuffer, width, startRow, endRow, (startCol + endCol) / 2, endCol);
     return list1;
 }
 
