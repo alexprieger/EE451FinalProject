51efb834d13bd6965f298821e5cb124e2aaeb0f2
Alex Prieger <prieger@usc.edu>
2023-04-27 21:20:12 -0700

    Added test cases

--- app/src/main/java/com/ajp/ee451finalproject/EdgeDetectionActivity.kt
+++ app/src/main/java/com/ajp/ee451finalproject/EdgeDetectionActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.ajp.ee451finalproject.databinding.ActivityEdgeDetectionBinding
 import java.io.BufferedInputStream
 import java.util.Vector
@@ -26,13 +27,18 @@ class EdgeDetectionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEdgeDetectionBinding
 
     private var imageFilePath: String? = null
+    private var imageSize: Int? = null
 
-    private val useGarciaMolla = true
+    private var useGarciaMolla = false
+    private var numThreads: Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         imageFilePath = intent.extras?.getString("image")
+        imageSize = intent.extras?.getInt("size")
+        useGarciaMolla = intent.extras?.getBoolean("useGarciaMolla") ?: false
+        numThreads = intent.extras?.getInt("numThreads")
 
         binding = ActivityEdgeDetectionBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -42,25 +48,20 @@ class EdgeDetectionActivity : AppCompatActivity() {
         super.onWindowFocusChanged(hasFocus)
 
         if (hasFocus && imageFilePath != null) {
-            val imageStream = BufferedInputStream(resources.assets.open("images/${imageFilePath}", ACCESS_BUFFER))
+            val imageStream = BufferedInputStream(resources.assets.open("images/${imageFilePath}/${imageSize}.png", ACCESS_BUFFER))
             val imageBitmap = BitmapFactory.decodeStream(imageStream)
-            val imageWidth = imageBitmap.width + 2 // + 2 to add black frame
-            val imageHeight = imageBitmap.height + 2
-
-            val imageWidthPadded = 2.0.pow(ceil(log2(imageWidth.toDouble()))).toInt()
-            val imageHeightPadded = 2.0.pow(ceil(log2(imageHeight.toDouble()))).toInt()
 
             // Image array is in row major order, with 0 representing black and 1 representing white
-            val imageArray = ByteArray(imageWidthPadded * imageHeightPadded) { i ->
-                val row = i / imageWidthPadded
-                val col = i % imageWidthPadded
-                if (row == 0 || row > imageBitmap.height || col == 0 || col > imageBitmap.width) {
+            val imageArray = ByteArray(imageBitmap.width * imageBitmap.height) { i ->
+                val row = i / imageBitmap.width
+                val col = i % imageBitmap.width
+                if (row == 0 || row == imageBitmap.height - 1 || col == 0 || col == imageBitmap.width - 1) {
                     // If on the frame or in the padding, make black
                     0
                 } else {
                     // Binarize by converting to black and white and thresholding at half (127)
                     val pixel = imageBitmap.getPixel(col - 1, row - 1)
-                    if (Color.red(pixel) + Color.blue(pixel) + Color.green(pixel) / 3 < 127) {
+                    if (Color.red(pixel) + Color.blue(pixel) + Color.green(pixel) / 3 < 128) {
                         // Transforming here between (x,y) coordinates and standard row-major matrix index ordering
                         // Also subtracting ones to deal with the frame
                         0
@@ -70,9 +71,9 @@ class EdgeDetectionActivity : AppCompatActivity() {
                 }
             }
             val edgeResult = if (useGarciaMolla) {
-                garciaMollaEdgeFindParallel(imageArray, imageWidthPadded, imageHeightPadded, 2)
+                garciaMollaEdgeFindParallel(imageArray, imageBitmap.width, imageBitmap.height, numThreads ?: 0)
             } else {
-                suzukiEdgeFind(imageArray, imageWidthPadded, imageHeightPadded)
+                suzukiEdgeFind(imageArray, imageBitmap.width, imageBitmap.height)
             }
             val edgeList = edgeResult.edges
             val edgeimageBitmap = imageBitmap.copy(Bitmap.Config.ARGB_8888, true)
@@ -80,7 +81,7 @@ class EdgeDetectionActivity : AppCompatActivity() {
             for (edge in edgeList) {
                 for (pixel in edge) {
                     // - 1s to subtract off the frame
-                    edgeimageBitmap.setPixel( pixel.col - 1,pixel.row - 1, Color.WHITE)
+                    edgeimageBitmap.setPixel( pixel.col,pixel.row, Color.WHITE)
                 }
             }
             val scaledEdgeImageBitmap = Bitmap.createScaledBitmap(edgeimageBitmap, binding.image.width, binding.image.height, false)
--- app/src/main/java/com/ajp/ee451finalproject/MainActivity.kt
+++ app/src/main/java/com/ajp/ee451finalproject/MainActivity.kt
@@ -1,22 +1,63 @@
 package com.ajp.ee451finalproject
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.View
 import android.view.View.OnClickListener
 import android.widget.Button
+import com.ajp.ee451finalproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        private val sizeArray = intArrayOf(4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048)
+        private val numThreadsArray = intArrayOf(0, 1, 2)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.sizeButton.text = sizeArray[0].toString()
+        binding.sizeButton.setOnClickListener { v ->
+            val button = v as Button
+            val currentSize = button.text.toString().toInt()
+            val indexOfCurrentSize = sizeArray.indexOf(currentSize)
+            val newSize = sizeArray[(indexOfCurrentSize + 1) % sizeArray.size]
+            button.text = newSize.toString()
+        }
+        binding.numThreadsButton.text = numThreadsArray[0].toString()
+        binding.numThreadsButton.setOnClickListener { v ->
+            val button = v as Button
+            val currentSize = button.text.toString().toInt()
+            val indexOfCurrentSize = numThreadsArray.indexOf(currentSize)
+            val newSize = numThreadsArray[(indexOfCurrentSize + 1) % numThreadsArray.size]
+            button.text = newSize.toString()
+        }
+        binding.useGarciaMollaButton.text = false.toString()
+        binding.useGarciaMollaButton.setOnClickListener { v ->
+            val button = v as Button
+            button.text = (!button.text.toString().toBoolean()).toString()
+        }
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
     }
 
     override fun onClick(v: View?) {
         val button = v as Button
         val edgeIntent = Intent(this, EdgeDetectionActivity::class.java).apply {
             putExtra("image", button.text)
+            putExtra("size", binding.sizeButton.text.toString().toInt())
+            putExtra("useGarciaMolla", binding.useGarciaMollaButton.text.toString().toBoolean())
+            putExtra("numThreads", binding.numThreadsButton.text.toString().toInt())
         }
         startActivity(edgeIntent)
     }
